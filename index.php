dd3a3dfa18e62d253cf47db7ec3e3ae3145691a7
Tomáš Hubina <hubina.tomas18@gmail.com>
2025-03-28 13:25:05 +0100

    head v php

--- index.php
+++ index.php
@@ -1,40 +1,11 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <title>Barista Cafe HTML CSS Template</title>
-
-        <!-- CSS FILES -->                
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,400;0,600;0,700;1,200;1,700&display=swap" rel="stylesheet">
-            
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
-
-        <link href="css/vegas.min.css" rel="stylesheet">
-
-        <link href="css/tooplate-barista.css" rel="stylesheet">
-        
-<!--
-
-Tooplate 2137 Barista Cafe
-
-https://www.tooplate.com/view/2137-barista-cafe
-
-Bootstrap 5 HTML CSS Template
-
--->
-    </head>
-    
+<?php
+    $file_path = "parts/head.php";
+if(!require($file_path)) {
+    echo"Failed to include $file_path";
+}
+?>  
     <body>
         <?php
             $file_path = "parts/header.php";
